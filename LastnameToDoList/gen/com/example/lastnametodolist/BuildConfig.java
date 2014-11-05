127ceaf3324078e29ad753e53d982bc8ff67f740
Christopher Michaud <cmichaud47@gatech.edu>
2014-11-05 13:54:09 -0500

    Part 1

---
+++ LastnameToDoList/gen/com/example/lastnametodolist/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.lastnametodolist;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LastnameToDoList/src/com/example/lastnametodolist/TasksDAO.java
@@ -0,0 +1,5 @@
+package com.example.lastnametodolist;
+
+public class TasksDAO {
+
+}
